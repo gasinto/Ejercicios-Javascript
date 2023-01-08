23c4e936108be8b3c2da42f3b5cde2a29334ae20
Juan Cruz Arcudi <arcudijuancruz@gmail.com>
2023-01-08 19:46:56 -0300

    Primer commit

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,4 @@
+// Ejercicio 1
+// Escribir un programa que muestre en pantalla ¡Hola Mundo!.
+
+console.log("¡Hola Mundo!");
---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,6 @@
+// Ejercicio 2
+// Escribir un programa que guarde en una variable ¡Hola Mundo! Y muestre esa variable por pantalla.
+
+var variable = "¡Hola Mundo!";
+
+console.log(variable);
